52c949b42bfaecb53981973a5b1cba3fd6cf1bdd
Cory Richard <chipjohnson1211@gmail.com>
2025-06-04 16:18:56 -0700

    Update main.js
    increase directional light to 2.5

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ controls.dampingFactor = 0.05;   // Damping factor
 // Lighting
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
 scene.add(ambientLight);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
 directionalLight.position.set(10, 10, 10);
 scene.add(directionalLight);
 
